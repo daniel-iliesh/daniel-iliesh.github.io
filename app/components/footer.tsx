52924e36e2bb40ab1c0987fbd40a7e6d14a3cbcf
Daniel Ilies <iliesh.daniel@gmail.com>
2025-07-21 00:42:26 +0400

    update

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -15,7 +15,10 @@ function ArrowIcon() {
   )
 }
 
-export default function Footer() {
+export default async function Footer() {
+  let data = await fetch('https://thebackend.rocket-champ.com/')
+  const ghProfile = await data.json()
+
   return (
     <footer className="mb-16">
       <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
@@ -35,7 +38,7 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://github.com/vercel/next.js"
+            href={ghProfile.html_url}
           >
             <ArrowIcon />
             <p className="ml-2 h-7">github</p>
@@ -46,7 +49,7 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://vercel.com/templates/next.js/portfolio-starter-kit"
+            href={`${ghProfile.html_url}/${ghProfile.blog}`}
           >
             <ArrowIcon />
             <p className="ml-2 h-7">view source</p>
@@ -54,7 +57,7 @@ export default function Footer() {
         </li>
       </ul>
       <p className="mt-8 text-neutral-600 dark:text-neutral-300">
-        © {new Date().getFullYear()} MIT Licensed
+        © Hi from {new Date().getFullYear()}!
       </p>
     </footer>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,49 @@
-import { BlogPosts } from 'app/components/posts'
+import { BlogPosts } from "app/components/posts";
+import { FaGithub, FaEnvelope, FaLinkedin } from "react-icons/fa";
+import ReadmeContent from "./components/readmeContent";
 
-export default function Page() {
+export default async function Page() {
+  let data = await fetch('https://thebackend.rocket-champ.com/')
+  const ghProfile = await data.json()
+
+  data = await fetch('https://thebackend.rocket-champ.com/readme')
+  const ghReadme = await data.text()
+
   return (
     <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
-      </h1>
+      <div className="flex items-center w-full gap-4 mb-8">
+        <div className="flex items-center gap-4 w-full">
+          <img src={ghProfile.avatar_url} alt="avatar" width={100} height={100} className="rounded-full border-2 border-gray-200" />
+          <div className="w-full gap-8">
+            <div className="flex justify-between w-full">
+              <h1 className="text-2xl font-semibold tracking-tighter">{ghProfile.name} - {ghProfile.bio}</h1>
+              <div className="flex gap-4 items-center">
+                <a href={ghProfile.html_url} target="_blank">
+                  <FaGithub color="white" size={24} className="hover:text-gray-300 align-top" />
+                </a>
+                <a href={`mailto:${ghProfile.email}`}>
+                  <FaEnvelope color="white" size={24} className="hover:text-gray-300 align-top" />
+                </a>
+                <a href={`https://linkedin.com/in/${ghProfile.login}`} target="_blank">
+                  <FaLinkedin color="white" size={24} className="hover:text-gray-300 align-top" />
+                </a>
+              </div>
+            </div>
+            <p className="text-sm text-gray-500">
+              {ghProfile.location} | {ghProfile.company}
+            </p>
+            <p className="text-sm text-gray-500">
+              {ghProfile.email}
+            </p>
+          </div>
+        </div>
+      </div>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        <ReadmeContent htmlContent={ghReadme} />
       </p>
       <div className="my-8">
         <BlogPosts />
       </div>
     </section>
-  )
+  );
 }
